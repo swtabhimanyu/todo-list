54e4ada09680f0493d079f97660942a8838d52c5
Abhimanyu Saraswat <swt.abhimanyu.31@gmail.com>
2020-06-02 13:45:00 +0530

    Add a controller and router

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express=require('express');
-const app=express();
 const port=8000;
+const app=express();
+
+
+app.use('/',require('./routes/index'));
+
 
 
 app.listen(port,function(err){
     if(err){
-        console.log(`Error in running server : ${err}`);  //interpolation
+        console.error(err);
         return;
     }
-    console.log(`Server is runing on port ${port}`);
+    console.log('Server up and running at port ', port);
 });
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+
+const router=express.Router();
+
+const homeController=require('../controllers/home_controller');
+
+// router.get('/',function(req,res){
+//     return res.end('<h1>Server up frands</h1>');
+// });
+
+router.get('/',homeController.home);
+
+console.log('Inside routes folder index.js');
+
+module.exports=router;
